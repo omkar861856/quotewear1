14ca872403b6445d6444bd922bdababab03e20b4
Omkar D Lolge <omkardlolge@Omkars-MacBook-Air-90.local>
2026-06-27 17:10:51 +0530

    Disable SSL in medusa-config

--- backend/medusa-config.ts
+++ backend/medusa-config.ts
@@ -8,6 +8,7 @@ loadEnv(process.env.NODE_ENV!, process.cwd());
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    databaseDriverOptions: { ssl: false },
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
